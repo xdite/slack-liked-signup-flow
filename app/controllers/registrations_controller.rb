6451589376783056a53efdf6ef87670043fd9142
xdite <xuite.joke@gmail.com>
2016-02-14 00:36:44 +0800

    add domain validation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -16,7 +16,7 @@ class RegistrationsController < Devise::RegistrationsController
 
 
   def sign_up_params
-    params.require(:user).permit(:name, :email, :teams_attributes => [ :name ])
+    params.require(:user).permit(:name, :email, :teams_attributes => [ :name, :domain ])
   end
 
   def account_update_params
