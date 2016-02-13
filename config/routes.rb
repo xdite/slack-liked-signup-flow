beebf095a9c0b711c727aff0411eda8bb40af7ad
xdite <xuite.joke@gmail.com>
2016-02-13 16:04:48 +0800

    welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
 
-  root 'pages#wecome'
+  root 'pages#welcome'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
