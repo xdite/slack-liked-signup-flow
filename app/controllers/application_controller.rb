7c08c2ce5ad000adc66ae576e72024e4f22b608f
xdite <xuite.joke@gmail.com>
2016-02-14 01:34:28 +0800

    Fix signout path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,12 @@ class ApplicationController < ActionController::Base
 
 
   include UrlHelper
-  
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    root_url(:subdomain => false )
+  end
+
 end
