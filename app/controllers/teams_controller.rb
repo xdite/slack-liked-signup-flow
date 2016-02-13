33ccc43f954f1d5731c0d5834a5aec6e641cf71b
xdite <xuite.joke@gmail.com>
2016-02-14 01:26:03 +0800

    find_team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,19 @@
 class TeamsController < ApplicationController
+  before_action :find_this_team
 
   def show
   end
+
+  protected
+
+
+  def find_this_team
+
+    @team = current_user.teams.find_by_domain(request.host.split('.').first)
+    # make sure with domain
+    # TODO: devise also need to check permission
+    # TODO: check redirect path
+
+  end
+
 end
